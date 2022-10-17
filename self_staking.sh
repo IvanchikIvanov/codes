248317f2b2ba3a483808caa2bece30e18a80f8ee
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-18 00:01:01 +0500

    Update self_staking.sh

--- self_staking.sh
+++ self_staking.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "Enter CHAIN : " 
-read CHAIN_ID
+if [ ! $CHAIN_ID ]; then
+	read -p "Enter CHAIN_ID: " CHAIN_ID
+	echo 'export CHAIN_ID='$CHAIN_ID >> $HOME/.bash_profile
+fi
 sleep 1
-echo "Enter DENOM: " 
-read DENOM
-sleep 1
+if [ ! $DENOM ]; then
+	echo 'export DENOM='$DENOM >> $HOME/.bash_profile
+fi
 a=$((RANDOM % 12)) 
 sleep $a
 b=`$CHAIN_ID keys show wallet --bech val`
