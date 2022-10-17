7d32f906492c86f82faba0274ecb3f205981548c
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-17 23:57:10 +0500

    Update self_staking.sh

--- self_staking.sh
+++ self_staking.sh
@@ -8,4 +8,4 @@ sleep 1
 a=$((RANDOM % 12)) 
 sleep $a
 b=`$CHAIN_ID keys show wallet --bech val`
-ollod  tx staking delegate $b 1000000$DENOM --from wallet --fees 500$DENOM -y
+ollod tx staking delegate $b 1000000$DENOM --from wallet --fees 500$DENOM -y
