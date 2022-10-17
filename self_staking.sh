74f72d93f9a9ef64d73d36c4af7513c0e5b8f175
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-18 00:18:01 +0500

    Update self_staking.sh

--- self_staking.sh
+++ self_staking.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 if [ ! $CHAIN_ID ]; then
-	read -p "Enter CHAIN_ID: " CHAIN_ID
-	echo 'export CHAIN_ID='$CHAIN_ID >> $HOME/.bash_profile
+	read -p "Enter CHAIN: " CHAIN
+	echo 'export CHAIN='$CHAIN >> $HOME/.bash_profile
 fi
 sleep 1
 if [ ! $DENOM ]; then
+	read -p "Enter DENOM: " DENOM
 	echo 'export DENOM='$DENOM >> $HOME/.bash_profile
+fi
+sleep 1
+if [ ! $ID ]; then
+	read -p "Enter ID: " ID
+	echo 'export ID='$ID >> $HOME/.bash_profile
 fi
 a=$((RANDOM % 12)) 
 sleep $a
-b=`$CHAIN_ID keys show wallet --bech val`
-$CHAIN_ID tx staking delegate $b 1000000$DENOM --from wallet --fees 500$DENOM -y
+b=`$CHAIN keys show wallet --bech val`
+$CHAIN tx staking delegate $b 1000000$DENOM --from wallet --chain-id=$ID --fees 500$DENOM -y
+
