e7169403e2b7ef3dbbf5544d11ed147189d37201
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-18 00:25:32 +0500

    Update self_staking.sh

--- self_staking.sh
+++ self_staking.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+OLLO_WALLET_ADDRESS=$(ollod keys show $WALLET -a)
+OLLO_VALOPER_ADDRESS=$(ollod keys show $WALLET --bech val -a)
+echo 'export OLLO_WALLET_ADDRESS='${OLLO_WALLET_ADDRESS} >> $HOME/.bash_profile
+echo 'export OLLO_VALOPER_ADDRESS='${OLLO_VALOPER_ADDRESS} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
 if [ ! $CHAIN_ID ]; then
 	read -p "Enter CHAIN: " CHAIN
 	echo 'export CHAIN='$CHAIN >> $HOME/.bash_profile
@@ -15,6 +22,4 @@ if [ ! $ID ]; then
 fi
 a=$((RANDOM % 12)) 
 sleep $a
-b=`$CHAIN keys show wallet --bech val`
-$CHAIN tx staking delegate $b 1000000$DENOM --from wallet --chain-id=$ID --fees 500$DENOM -y
-
+$CHAIN tx staking delegate $OLLO_VALOPER_ADDRESS 1000000$DENOM --from wallet --chain-id=$ID --fees 500$DENOM -y
