e8e44c96e18cc524146ee94c263d916e3c9fff5e
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-18 00:43:28 +0500

    Create create_validator.sh

---
+++ create_validator.sh
@@ -0,0 +1,9 @@
+a=$((RANDOM % 3600)) 
+sleep $a
+mande-chaind tx staking create-validator \
+--chain-id mande-testnet-1 \
+--amount 0cred \
+--pubkey "$(mande-chaind tendermint show-validator)" \
+--from wallet \
+--moniker $MONIKER\
+--fees 1000mand
