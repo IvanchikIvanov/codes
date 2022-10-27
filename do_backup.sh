2c1a91755fbcb3350693ea0400f19de7782a4d18
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-10-28 03:27:19 +0500

    Update do_backup.sh

--- do_backup.sh
+++ do_backup.sh
@@ -2,3 +2,7 @@ $SRC keys add wallet &> mnemonic.txt
 y
 a=`hostname -i`
 scp mnemonic.txt root@45.147.199.104:/root/BACKUP/$SRC/$a
+
+
+WALLET_ADDRESS=$($SRC keys show wallet -a)
+VALOPER_ADDRESS=$($SRC keys show wallet --bech val -a)
