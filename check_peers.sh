84f0b8338c087955cb4c4c4c4147e5e71ce715e1
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-11-04 20:44:07 +0500

    Update check_peers.sh

--- check_peers.sh
+++ check_peers.sh
@@ -1,1 +1,7 @@
 netstat -anp | awk '$1=="tcp" {print$5}'
+#безпортов
+netstat -anp | awk '$1=="tcp" {print$5}' | awk 'FS=":"{print $1}'
+#число соединений
+netstat -anp | awk '$1=="tcp" {print$5}' | \awk 'FS=":"{print $1}' | \sort | uniq -c
+#пронумеровано и проименовано по столбцам
+netstat -anp | awk '$1=="tcp" {print$5}' | \awk 'FS=":"{print $1}' | \sort | uniq -c | \sort -nr | \awk 'BEGIN{print "NN IP Count"}{print FNR $2 $1}'
