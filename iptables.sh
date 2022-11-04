1b64611c810fe3da983cacafa51b181e1111e88c
Zuxell <86493242+IvanchikIvanov@users.noreply.github.com>
2022-11-04 22:56:04 +0500

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,4 +1,4 @@
 #закрыть ввод пакетов, с открытым ответом на выходные пакеты
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-#принимать пакеты самому от себя
+#принимать пакеты на локальный интерфейс(самому от себя)
 iptables -A INPUT -i lo -j ACCEPT
